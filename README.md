e94f46d310f61e8679b8f171ead9c6814df65e1c
mattssonmart <mattsson112@hotmail.com>
2025-10-22 10:17:22 +0200

    woohoo

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # workshop-dad
+
+skapar variable jokeElement och pekar på joke
+skapar variable jokeBtn och pekar på jokeBtn
+
+generera skämt
+
+funktion för att skhämta slumpat skämt skämt
+
+
+
---
+++ app.js
@@ -0,0 +1,19 @@
+const jokeElement = document.getElementById('joke');
+const jokeBtn = document.getElementById('jokeBtn');
+
+generateJoke();
+
+function generateJoke() {
+    const config = {
+        headers: {
+            accept: 'application/json',
+        },
+    };
+    fetch('https://icanhazdadjoke.com', config)
+    .then((res) => res.json())
+    .then((data) => {
+        jokeElement.innerHTML = data.joke;
+    });
+}
+
+jokeBtn.addEventListener('click', generateJoke);
